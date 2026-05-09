143d81af411d5613ccd441d2271991501be3e016
Gautam Lathiya <lathiyagautam18@gmail.com>
2026-05-09 20:22:52 +0530

    fix: workflow image in mobile devices

--- src/pages/Workflow/WorkflowImage.tsx
+++ src/pages/Workflow/WorkflowImage.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 
 const WorkflowImage = () => {
   return (
-    <div className="flex justify-center py-16">
+    <div className="flex justify-center py-8 md:py-16 px-3 sm:px-4">
         <img
           src="/workflow.png"
           alt="Workflow Visual"
-          className="rounded-xl shadow-lg max-w-full h-auto"
-          style={{ maxWidth: 600 }}
+          className="w-full max-w-5xl h-auto rounded-xl shadow-lg object-contain"
         />
     </div>
   )
